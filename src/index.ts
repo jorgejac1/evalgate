738605af386a8d1420cc446e1101e8fe63d24f91
Jorge Jacome <jorge2002_mx@hotmail.com>
2026-04-18 00:26:27 -0400

    feat: export swarm/worktree/spawn internals from public index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,18 @@ export {
 } from "./memory.js";
 export { listMessages, sendMessage } from "./messages.js";
 export { parseTodo } from "./parser.js";
+export type { SpawnOpts } from "./spawn.js";
+export { spawnAgent } from "./spawn.js";
+export type { SwarmOptions, SwarmResult } from "./swarm.js";
+export { retryWorker, runSwarm, swarmEvents } from "./swarm.js";
+export { loadState, saveState, updateWorker } from "./swarm-state.js";
 export type {
 	AgentMessage,
 	BudgetRecord,
 	CompositeVerifier,
 	Contract,
 	ContractTrigger,
+	GatewayConfig,
 	LlmVerifier,
 	McpCapabilities,
 	McpJsonRpcRequest,
@@ -42,12 +48,24 @@ export type {
 	ScheduleTrigger,
 	ShellVerifier,
 	Status,
+	SwarmState,
+	TelegramMessage,
+	TelegramUpdate,
 	TriggerSource,
 	Verifier,
 	WatchTrigger,
 	WebhookTrigger,
+	WorkerState,
+	WorkerStatus,
 } from "./types.js";
 export { startUiServer } from "./ui.js";
 export { runContract, runShell } from "./verifier.js";
 export { matchesGlob, startWatcher } from "./watcher.js";
+export {
+	createWorktree,
+	deleteBranch,
+	getRepoRoot,
+	mergeWorktree,
+	removeWorktree,
+} from "./worktree.js";
 export { updateTodo } from "./writer.js";
